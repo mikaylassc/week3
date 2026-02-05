394a4a991954fc411bba35056ae1a40a74328966
mikaylassc <mikaylassc@gmail.com>
2026-02-05 14:51:00 -0500

    Add nested loop star pattern
    nested loops printing exactly 5 rows

---
+++ star_pattern.py
@@ -0,0 +1,14 @@
+"""
+Mikayla Settles-Chambers
+CMSC 111
+Spring 2026
+Assignment 2 Week 3 (Nested Loops)
+"""
+'''
+#nested loops are used to print the stars pattern depending on the value of x
+'''
+x = 5
+for i in range (1, x + 1):
+    for j in range (i):
+        print("*")
+    print()
