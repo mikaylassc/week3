53b651080569e11e2b6671bfcd320d0f09ca966d
mikaylassc <mikaylassc@gmail.com>
2026-02-05 14:26:40 -0500

    Create week3_loop.py
    Assignment 1 for loops

---
+++ week3_loop.py
@@ -0,0 +1,12 @@
+"""
+Mikayla Settles-Chambers
+CMSC 111
+Spring 2026
+Assignment 1 Week 3 (Loops)
+"""
+'''
+#chose for loop because range is met easily in sequence
+'''
+for i in range (1,51):
+    print (i)
+    
